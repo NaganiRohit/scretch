38212ba2bcffdcb2d58efd14fa5320ade19c5a94
NaganiRohit <ajaybaraiya833@gmail.com>
2024-06-04 08:03:30 -0700

    21st may - added separation ofconcerns in models and created owner model

---
+++ app.js
@@ -0,0 +1,28 @@
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const app = express();
+const db = require("./config/mongoose-connection")
+const path= require('path');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname,"public")));
+app.set("view engine","ejs");
+
+const ownersRouter= require('./routes/ownersRouter');
+const usersRouter= require('./routes/usersRouter');
+const productsRouter= require('./routes/productsRouter');
+
+
+
+app.get("/",(req,res)=>{
+    res.send("hey")
+});
+
+app.use("/owners", ownersRouter)
+app.use("/users", usersRouter)
+app.use("/products", productsRouter)
+
+
+app.listen(3000);
